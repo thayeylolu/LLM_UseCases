ae373724b17ffa2991b814b6ad77bd7108856e45
Taiwo Owoseni <hamdallahhassanah@gmail.com>
2026-01-07 22:28:22 -0500

    creates file structure

---
+++ Wellness/template.sh
@@ -0,0 +1,21 @@
+# create directory
+mkdir  -p src
+mkdir -p research
+
+# create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+
+touch research/trials.ipynb
+touch research/__init__.py
+touch research/analysis.py
+
+touch .env
+tocuh setup.py
+touch app.py
+touch README.md
+touch requirements.txt
+
+echo "Direcrtory structure for Wellness project created."
+
